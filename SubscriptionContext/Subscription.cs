b0010aacab562b34492410fbc9eabc76bd38fd5c
louresb <bruno.loures@hotmail.com>
2023-03-04 23:44:51 -0300

    Fundamentos da orientação a objetos

---
+++ SubscriptionContext/Subscription.cs
@@ -0,0 +1,18 @@
+using System;
+using Balta.SharedContext;
+
+namespace Balta.SubscriptionContext
+{
+    public class Subscription : Base
+    {
+        public Plan Plan { get; set; }
+        public DateTime? EndDate { get; set; }
+
+        public bool IsInactive => EndDate <= DateTime.Now;
+
+        internal static bool Any(Func<object, object> value)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
